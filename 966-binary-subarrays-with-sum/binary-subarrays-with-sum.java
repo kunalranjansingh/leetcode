8b4a3cc523234e513d43a82e36a43a144e322801
Kunal Singh <kunalranjan677@gmail.com>
2026-09-13 11:19:34 +0530

    Time: 23 ms (33.23%) | Memory: 52.7 MB (15.06%) - LeetSync

---
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        int count = 0;
+        int sum =0;
+        HashMap<Integer , Integer> map = new HashMap<>();
+        map.put(0,1);
+
+        for(int num : nums){
+            sum += num;
+            int required = sum - goal;
+            if(map.containsKey(required)){
+                count += map.get(required);
+            }
+            map.put(sum , map.getOrDefault(sum , 0)+1);
+        }
+        return count;
+    }
+}
