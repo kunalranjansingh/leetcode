aef47c010f93100a98c2b827a38a4668d964cf33
Kunal Singh <kunalranjan677@gmail.com>
2026-09-21 19:38:40 +0530

    Time: 62 ms (34.52%), Space: 48.6 MB (6.71%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.java
+++ 0846-hand-of-straights/0846-hand-of-straights.java
@@ -4,9 +4,18 @@ class Solution {
             return false;
         }
         TreeMap<Integer , Integer> map = new TreeMap<>();
+        //HashMap<Integer , Integer> map = new HashMap<>();
         for(int i=0; i<hand.length; i++){
             map.put(hand[i], map.getOrDefault(hand[i],0)+1);
         }
+        //for(int currentCard: hand){
+          //  if(!map.containsKey(currentCard)){
+            //    continue;
+            //}
+            //while(map.containsKey(currentCard-1)){
+              //  currentCard--;
+           // }
+        
         while(map.size()>0){
             int currentCard = map.entrySet().iterator().next().getKey();
             for(int i=0; i<groupSize; i++){
