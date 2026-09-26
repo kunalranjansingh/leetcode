10c7b997298a1dbc4506fbfae5cfcf532d189f8b
Kunal Singh <kunalranjan677@gmail.com>
2026-09-26 18:51:58 +0530

    Time: 1 ms (100.00%) | Memory: 46.4 MB (76.85%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -9,34 +9,59 @@
  * }
  */
 class Solution {
+    public ListNode add(ListNode l1, ListNode l2 , int carry){
+        if (l1 == null && l2 == null && carry == 0){
+            return null;
+        }
+        int sum = carry;
+        if (l1 != null){
+            sum += l1.val;
+            l1 = l1.next;
+        }
+
+        if(l2 != null){
+            sum += l2.val;
+            l2 = l2.next;
+        }
+
+        int digit = sum % 10;
+        carry = sum / 10;
+
+        ListNode dummy = new ListNode (digit);
+        dummy.next = add(l1, l2 , carry);
+
+        return dummy;
+    }
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode result = new ListNode(0);
-        ListNode dummy = result;
 
-        int carry = 0;
+        return add(l1, l2 ,0);
+        // ListNode result = new ListNode(0);
+        // ListNode dummy = result;
 
-        while(l1 != null || l2 != null){
+        // int carry = 0;
 
-            int sum = 0 + carry;
+        // while(l1 != null || l2 != null){
 
-            if(l1 != null){
-                sum += l1.val;
-                l1=l1.next;
-            }
-            if(l2 != null){
-                sum += l2.val;
-                l2 = l2.next;
-            }
-            carry = sum / 10;
-            sum = sum % 10;
+        //     int sum = 0 + carry;
 
-            dummy.next = new ListNode(sum);
-            dummy = dummy.next;
-        }
-        if(carry == 1){
-            dummy.next = new ListNode(1);
+        //     if(l1 != null){
+        //         sum += l1.val;
+        //         l1=l1.next;
+        //     }
+        //     if(l2 != null){
+        //         sum += l2.val;
+        //         l2 = l2.next;
+        //     }
+        //     carry = sum / 10;
+        //     sum = sum % 10;
+
+        //     dummy.next = new ListNode(sum);
+        //     dummy = dummy.next;
+        // }
+        // if(carry == 1){
+        //     dummy.next = new ListNode(1);
             
-        }
-        return result.next;
+        // }
+        // return result.next;
     }
 }
